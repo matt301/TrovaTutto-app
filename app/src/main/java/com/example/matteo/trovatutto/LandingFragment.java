b5ca44b0c9a407b91f9ccfb09a4003b0a865b8a7
matt301 <s3911811@studenti.unige.it>
2017-10-19 16:39:17 +0200

    Inseriti i fragments per landing page e login/registrazione

---
+++ app/src/main/java/com/example/matteo/trovatutto/LandingFragment.java
@@ -0,0 +1,45 @@
+package com.example.matteo.trovatutto;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LandingFragment extends Fragment {
+
+    private OnSelectionListener listener;
+
+    public interface OnSelectionListener{
+
+    }
+        public LandingFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_login, container, false);
+        return view;
+    }
+
+/*
+    @Override
+    public void onAttach(Activity activity) {
+
+    }
+
+    @Override
+    public void onDetach() {
+
+    }
+  */
+
+}
