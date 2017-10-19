5d0b8e4b5ac43f2de3bd462523f6337e831cf8ae
matt301 <s3911811@studenti.unige.it>
2017-10-19 17:49:33 +0200

    Inseriti i fragments per landing page e login/registrazione

--- app/src/main/java/com/example/matteo/trovatutto/LandingFragment.java
+++ app/src/main/java/com/example/matteo/trovatutto/LandingFragment.java
@@ -26,7 +26,7 @@ public class LandingFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.activity_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_landing_fragments, container, false);
         return view;
     }
 
--- app/src/main/java/com/example/matteo/trovatutto/LoginActivity.java
+++ app/src/main/java/com/example/matteo/trovatutto/LoginActivity.java
@@ -19,6 +19,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.TextUtils;
+
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -38,6 +39,10 @@ import static android.Manifest.permission.READ_CONTACTS;
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
+
+
+
+
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -66,6 +71,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         // Set up the login form.
         mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
         populateAutoComplete();
@@ -93,6 +99,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
+
+
 
     private void populateAutoComplete() {
         if (!mayRequestContacts()) {
