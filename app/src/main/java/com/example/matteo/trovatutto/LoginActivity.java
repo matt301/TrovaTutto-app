f00b0e2a990dd33bbff27a43ce33c6713295a27b
Andrea941 <raineri.andrea94@gmail.com>
2017-10-25 11:22:26 +0200

    Fragment--->HomeActivity

--- app/src/main/java/com/example/matteo/trovatutto/LoginActivity.java
+++ app/src/main/java/com/example/matteo/trovatutto/LoginActivity.java
@@ -3,10 +3,12 @@ package com.example.matteo.trovatutto;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginActivity extends  Activity{
@@ -26,7 +28,6 @@ public class LoginActivity extends  Activity{
             fragmentTransaction.commit();
 
         }
-
 
 
     }
--- app/src/main/java/com/example/matteo/trovatutto/LoginFragment.java
+++ app/src/main/java/com/example/matteo/trovatutto/LoginFragment.java
@@ -1,11 +1,13 @@
 package com.example.matteo.trovatutto;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class LoginFragment extends Fragment {
@@ -15,7 +17,26 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+
+        Button btHome = (Button)view.findViewById(R.id.email_sign_in_button);
+        btHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Intent openHome = new Intent(getActivity(),HomeActivity.class);
+                startActivity(openHome);
+            }
+        });
+
         return view;
+
+
     }
+
+
+
+
+
+
+
 
 }
