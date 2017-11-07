3eefcff2f9f589ce8f3782bd359c35a11cec49b8
alessiozignaigo <alessio.zignaigo@yahoo.it>
2017-11-07 09:34:41 +0100

    TODO

---
+++ app/src/main/java/com/example/matteo/trovatutto/TODO.java
@@ -0,0 +1,17 @@
+package com.example.matteo.trovatutto;
+
+/**
+ * Created by aless on 07/11/2017.
+ */
+/*TODO:-Upload information profile ( I campi lasciati vuoi non verrano modificati )
+        -Button Home from provv Profile
+        -Home Segnalazioni
+        -Crea Segnalazioni
+        -Side NavBar with : Home, Crea Segnalazione, ProfileDati, ProfileMieSegnalazioni
+        -Google contact
+        -Notifiche
+        -Geolocalizzazione
+        -Pubblicazione su FB
+
+
+*/
