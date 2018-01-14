eaa8a98f997dc6bc580caabc04ed064beeaff29b
Alessio <s4094311@studenti.unige.it>
2018-01-14 11:40:49 +0100

    todo

--- app/src/main/java/com/example/matteo/trovatutto/TODO.java
+++ app/src/main/java/com/example/matteo/trovatutto/TODO.java
@@ -2,15 +2,23 @@ package com.example.matteo.trovatutto;
 
 /*
 
-    TODO:   -AutoCrop Image
-    TODO:   -Google contact
-    TODO:   -Notifiche(Geolocalizzate?)
-    !TODO:   -Cancellare Search bar mie seganlazioni
-            -caricamento miei report
-            -caricamento report home quando deleti
-            -pulsante close profilo pubblico
-            -titolo
-            -togliere snackbar
+       TODO:   -Feature Aggiuntiva : Segnalazioni nel raggio di .. da te
+       TODO:   -Salvare contatto in rubrica
+
+
+       TODO:   -Login Tramite G+
+       TODO:   -Cancellare SearchBar mie seganlazioni (provare se funziona) , profilo , new report
+       TODO:   -Titolo --> Lingua                                                                                       A
+       TODO:   -Togliere snackbar
+       TODO:   -Grafica                                                                                                 A
+       TODO:   -Dopo registrazaione andare al login
+       TODO:   -Data in sovraimpressione 0000-00-00                                                                     A
+       TODO:   -AutoComplete Indirizzo New Segnalazione
+       TODO:   -Caricamento doppio delle mie segnalazioni, secondo caricamento in barra caricamento, caricamento
+
+
+       TODO:   -Scaricare solo un tot e scaricarne altre quando sei in un punto
+
 
 
         AIzaSyCjNpkYHoKFbjKnZ5Hzhvgfk4dI24qOQN8 Api Key
@@ -22,10 +30,13 @@ package com.example.matteo.trovatutto;
 
         Login con Google Plus (opzionale)
         https://www.androidhive.info/2014/02/android-login-with-google-plus-account-1/
+        https://developers.google.com/identity/sign-in/android/
+
         Per notifiche
         https://www.androidhive.info/2016/02/android-push-notifications-using-gcm-php-mysql-realtime-chat-app-part-1/
         Chat
         https://www.androidhive.info/2016/02/android-push-notifications-using-gcm-php-mysql-realtime-chat-app-part-1/
+
         Mappe(un po' vecchie)
         https://www.androidhive.info/2013/08/android-working-with-google-maps-v2/
 
