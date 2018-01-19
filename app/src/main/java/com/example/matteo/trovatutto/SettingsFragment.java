55dcf80bff97bd0f8e70f5d3fa33456a1ad155f4
Andrea941 <raineri.andrea94@gmail.com>
2018-01-19 15:31:04 +0100

    Try this shit <3

---
+++ app/src/main/java/com/example/matteo/trovatutto/SettingsFragment.java
@@ -0,0 +1,39 @@
+package com.example.matteo.trovatutto;
+
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SettingsFragment extends PreferenceFragment {
+
+
+    public SettingsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = super.onCreateView(inflater, container, savedInstanceState);
+        view.setBackgroundColor(Color.WHITE);
+
+        return view;
+    }
+
+
+}
